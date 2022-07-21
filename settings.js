85dac4582a98b09a8df3373f637233ea38464fcd
ayoub-elkhadir <108630240+ayoub-elkhadir@users.noreply.github.com>
2022-07-21 14:53:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,12 +19,12 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['212617076646']
+global.premium = ['212617076646']
+global.ownernomer = '212617076646'
+global.ownername = 'ayoub elkhadir'
+global.botname = 'ayoub'
+global.footer = 'ayoub '
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
